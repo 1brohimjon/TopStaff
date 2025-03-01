92f1f7411c5e6f1fc3ea0d692570817ed09a10c0
1brohimjon <ibrohimjontursunov110@gmail.com>
2025-03-01 12:35:22 +0500

    The foundation has been created
    The foundation has been created

---
+++ topstaff/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-black flex items-center justify-start">
+      <img src="../logo-topstaff.png" alt="" width={90}/>
+      <h1 className='text-4xl text-emerald-400 font-bold'>TopStaff</h1>
+    </div>
+  )
+}
+
+export default App
